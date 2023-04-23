c47ac98207928f7a6d05f8c1f60282c0ba402eac
sayanok <rng.jhn.osca@gmail.com>
2023-04-23 12:10:08 +0900

    Specifications study

--- notes-app/src/App.tsx
+++ notes-app/src/App.tsx
@@ -1,14 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const App: React.FC = () => {
+  const [listOfNotes, setListOfNotes] = useState<Array<string>>(["aaa", "bbb", "ccc"]);
+
   return (
     <>
       {/* ページ上にメモ新規作成ボタン */}
+      <button>新規作成</button>
+      {/* 新規作成の画面が必要 */}
+
       {/* メモ一覧 */}
-      {/* 一覧の横にメモ表示ボタンとメモ編集ボタンとメモ削除ボタン */}
+      <div>
+        {listOfNotes.map((note) => (
+          <li>
+            <a href={note}>{note}</a>
+            <button>削除</button>
+          </li>
+        ))}
+      </div>
+      {/* 一覧の横にメモ表示ボタンとメモ削除ボタン */}
 
       {/* メモ表示ボタン→メモ詳細ページ行く？
+      メモ詳細ページにはメモ表示ボタン設置
       マークダウンするなら一言メモじゃないよな... */}
     </>
   );
